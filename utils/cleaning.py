778a1da991ce273dd68c836dd0f43a98c1ba5b1a
vitorccmanso <vitorccmanso@hotmail.com>
2024-02-03 18:29:35 -0300

    Documenting functions

--- utils/cleaning.py
+++ utils/cleaning.py
@@ -2,14 +2,12 @@ import pandas as pd
 
 def calculate_whiskers(column):
     """
-    Calculate upper and lower whiskers for identifying potential outliers in a dataset.
-
+    Calculate upper and lower whiskers for identifying potential outliers in a dataset
     Parameters:
-    - column (pandas.Series): The column for which whiskers are to be calculated.
-
+    - column (pandas.Series): The column for which whiskers are to be calculated
     Returns:
-    - upper_whisker (float): The upper whisker value.
-    - lower_whisker (float): The lower whisker value.
+    - upper_whisker (float): The upper whisker value
+    - lower_whisker (float): The lower whisker value
     """
     q1 = column.quantile(0.25)
     q3 = column.quantile(0.75)
--- utils/visualizations.py
+++ utils/visualizations.py
@@ -2,13 +2,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def create_subplots(rows, columns, figsize=(18,12)):
-    """Creates a figure and subplots with common settings."""
+    """
+    Creates a figure and subplots with common settings
+    Parameters:
+    - rows: Number of rows in the subplot grid
+    - columns: Number of columns in the subplot grid
+    - figsize: Tuple specifying the width and height of the figure (default is (18, 12))
+    Returns:
+    - fig: The Matplotlib figure object
+    - ax: A 1D NumPy array of Matplotlib axes
+    """
     fig, ax = plt.subplots(rows, columns, figsize=figsize)
     ax = ax.ravel()
     return fig, ax
 
 def plot_columns(data, cols, plot_func, ax, title_prefix="", x=None):
-    """Plots specified columns using the given plotting function
+    """
+    Plots specified graphs using the given plotting function
+    Parameters:
+    - data: DataFrame containing the data to be plotted
+    - cols: List of column names to be plotted
+    - plot_func: The Seaborn plotting function to be used
+    - ax: Matplotlib axes to plot on
+    - title_prefix: Prefix to be added to the title of each subplot (default is an empty string)
+    - x: Optional parameter for the x-axis when plotting scatter plots (default is None)
     """
     for i, col in enumerate(cols):
         if plot_func == sns.boxplot:
@@ -22,13 +39,27 @@ def plot_columns(data, cols, plot_func, ax, title_prefix="", x=None):
         ax[i].set_title(f"{title_prefix}{col.capitalize()}")
 
 def remove_unused_axes(fig, ax, num_plots):
-    """Removes unused axes from a figure."""
+    """
+    Removes unused axes from a figure
+    Parameters:
+    - fig: The Matplotlib figure object
+    - ax: A 1D NumPy array of Matplotlib axes
+    - num_plots: Number of subplots to keep; remove the rest
+    """
     total_axes = len(ax)
     for j in range(num_plots, total_axes):
         fig.delaxes(ax[j])
 
 def numerical_univariate_analysis(data, rows, columns):
-    """Performs univariate analysis on numerical columns."""
+    """
+    Performs univariate analysis on numerical columns
+    Parameters:
+    - data: DataFrame containing numerical data for analysis
+    - rows: Number of rows in the subplot grid
+    - columns: Number of columns in the subplot grid
+    Effect:
+    - Plots the distribution of numerical columns using seaborn's histplot
+    """
     fig, ax = create_subplots(rows, columns)
     cols = data.select_dtypes(include="number")
     plot_columns(data, cols, sns.histplot, ax, title_prefix="Distribution of ")
@@ -38,7 +69,15 @@ def numerical_univariate_analysis(data, rows, columns):
 
 
 def categorical_univariate_analysis(data, rows, columns):
-    """Performs univariate analysis on categorical columns."""
+    """
+    Performs univariate analysis on categorical columns
+    Parameters:
+    - data: DataFrame containing categorical data for analysis
+    - rows: Number of rows in the subplot grid
+    - columns: Number of columns in the subplot grid
+    Effect:
+    - Plots countplots for each categorical column
+    """
     fig, ax = create_subplots(rows, columns)
     cols = data.select_dtypes(include="object")
     plot_columns(data, cols, sns.countplot, ax)
@@ -47,7 +86,16 @@ def categorical_univariate_analysis(data, rows, columns):
     plt.show()
 
 def features_vs_targets(data, rows, columns):
-    """Plots numerical features against the target variable."""
+    """
+    Plots numerical features against the target variable
+    Parameters:
+    - data: DataFrame containing both features and target variable
+    - rows: Number of rows in the subplot grid
+    - columns: Number of columns in the subplot grid
+    Effect:
+    - Plots boxplots for numerical features against the target variable
+    - Plots a countplot for the target variable against the 'type' column
+    """
     fig, ax = create_subplots(rows, columns, figsize=(18, 12))
     cols = data.drop(columns=["target"]).select_dtypes(include="number")
     plot_columns(data, cols, sns.boxplot, ax, "Target x ")
@@ -61,14 +109,32 @@ def features_vs_targets(data, rows, columns):
     plt.show()
 
 def facegrid_hist_target(df, facecol, color):
+    """
+    Generates FacetGrid histograms for numerical columns based on target values
+    Parameters:
+    - df: DataFrame containing data for analysis
+    - facecol: Column for creating facets in the FacetGrid
+    - color: Color for the histograms
+    Effect:
+    - Creates a FacetGrid for each column based on the unique values in the specified facecol
+    - Filters the data to include only rows where the "target" column is equal to 1
+    - Shows the resulting FacetGrids with histograms
+    """
     for col in df.drop(columns=["target"]).select_dtypes(include="number"):
         g = sns.FacetGrid(df[df["target"]==1], col=facecol)
         g.map(sns.histplot, col, color=color)
         plt.show()
 
 def plot_scatter_numericals_target(data, rows, columns, x):
-    """Plots scatter plots of numerical columns against tool_wear,
-       optionally filtering by target value.
+    """
+    Plots scatter plots of numerical columns against x column for target value 1
+    Parameters:
+    - data: DataFrame containing data for analysis
+    - rows: Number of rows in the subplot grid
+    - columns: Number of columns in the subplot grid
+    - x: Column for the x-axis in scatter plots
+    Effect:
+    - Plots scatter plots using seaborn's scatterplot for each numerical column against x column
     """
     fig, ax = create_subplots(rows, columns, figsize=(18, 12))
     cols = data.drop(columns=["target", x]).select_dtypes(include='number')
