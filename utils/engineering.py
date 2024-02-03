3a48b0e5bbbfd2764e8b9f818e61ec40b05b9a70
vitorccmanso <vitorccmanso@hotmail.com>
2024-02-03 20:21:44 -0300

    Adding docstrings to the functions

--- utils/engineering.py
+++ utils/engineering.py
@@ -4,10 +4,28 @@ from sklearn.model_selection import train_test_split
 import os
 
 def plot_corr(data, method):
+    """
+    Plots a heatmap of the correlation matrix for numerical columns in the DataFrame
+    Parameters:
+    - data: DataFrame for correlation analysis
+    - method: Correlation method to use ('pearson', 'kendall', or 'spearman')
+    """
     sns.heatmap(data.select_dtypes(include="number").corr(method=method), annot=True, cmap="RdYlGn")
     plt.show()
 
 def saving_pipeline(data, save_folder, save_filename, test_size, random_state):
+    """
+    Splits the dataset into train and test sets, saves them, and creates the specified folder if it doesn't exist
+    Parameters:
+    - data: DataFrame containing the original dataset
+    - save_folder: Folder path where the datasets will be saved
+    - save_filename: Base filename for the saved datasets
+    - test_size: Proportion of the dataset to include in the test split
+    - random_state: Seed for reproducibility
+    Effect:
+    - Creates the specified folder if it doesn't exist
+    - Saves the original dataset, train set, and test set as CSV files with appropriate filenames
+    """
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
     
